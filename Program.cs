64670e7f6d6fb2e1a39c29d2127d4b9024108770
epdsn <ibestudios@gmail.com>
2020-07-02 21:29:58 -0700

    refactored code into seperate methods.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CsvHelper;
+using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
 using System.Linq;
@@ -12,52 +13,103 @@ namespace CodeExcerciseDataImportOutput
         {
             
             string fileLocation;
-            string fileType; 
-            string _numberOfFields;
+            //string fileType; 
+            int numberOfFields;
             string[] fileData = null;
+            string delimiter;
+            var dataFields = new object();
 
-            Console.WriteLine("Where is the file located?");
-            fileLocation = Console.ReadLine();
 
-            Console.WriteLine("\r\nIs the file format CSV (comma-seperated values) or TSV (tab seperated values)?\r\n- Enter 1 for CSV Or Enter 2 for TSV:");
-            fileType = Console.ReadLine();
-
-            Console.WriteLine("\r\nHow many fields should each record contain?");
-            _numberOfFields =  Console.ReadLine();
-
-            try
+            int count = 0;
+            do
             {
-                fileData = File.ReadAllLines(@fileLocation);
-            }
-            catch( Exception ex ) {
-                Console.WriteLine("\r\nThere was an error retrieving file.");
-            }
-
-            var dataFields = from row in fileData
-                            let data = row.Split(',')
-                            select new
-                            {
-                                FirstName = data[0],
-                                MiddleName = data[1],
-                                LastName = data[2]
-                            };
-
+                if (count > 0) {
+                    Console.WriteLine("\r\nFile does not exist OR you do not sufficant permissions.\r\nPlease enter a valid file path:");
+                }
+                count++;
+                Console.WriteLine("\r\nWhere is the file located?");
+                fileLocation = Console.ReadLine();
+            } while (!File.Exists(fileLocation));
 
             
+            Console.WriteLine("\r\nIs the file format CSV (comma-seperated values) or TSV (tab seperated values)?\r\n- Enter 1 for CSV Or Enter 2 for TSV:");
+            var choice = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("output:\r\n--------------------------");
-            var csv = new StringBuilder();
-            foreach (var data in dataFields.Skip(1))
+            delimiter = (choice == 1) ? "," : "\t";
+
+            Console.WriteLine("\r\nHow many fields should each record contain?");
+            numberOfFields = Convert.ToInt32(Console.ReadLine());
+
+
+            fileData = readData(@fileLocation);
+
+            ReadRecords(fileData, delimiter, numberOfFields );
+
+
+            Console.WriteLine("\r\nExport Complete");
+            Console.ReadLine();
+
+        }
+
+        public static string[] readData(string filepath)
+        {
+            try
             {
-                var newLine = $"{data.FirstName},{data.MiddleName},{data.LastName}";
-                csv.AppendLine(newLine);
+                return File.ReadAllLines(@filepath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was an error reading the file.");
+                throw new ApplicationException("Error retrieving record:", ex);
+            }
+        }
+
+
+        public static void ReadRecords(string[] data, string delimiter, int numOfFields)
+        {
+
+            string[] invalidFormat = { "Record is not in a valid format" };
+            try
+            {
+
+                var header = data[0].Split(delimiter);
+
+                var csv = new StringBuilder();
+                foreach (var row in data.Skip(1))
+                {
+                    string[] recordFields = row.Split(delimiter);
+
+                    if (recordFields.Count() == numOfFields)
+                    {
+                        csv.AppendLine(row);
+                    }
+
+                    //var newLine = $"{recordFields[0]},{recordFields[1]},{recordFields[2]}";
+                    //csv.AppendLine(newLine);
+                }
+                File.WriteAllText(@"newfile3.csv", csv.ToString());
 
             }
-            File.WriteAllText(@"newfile3.csv", csv.ToString());
-            Console.WriteLine("--------------------------");
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was an error reading the record.");
+                throw new ApplicationException("Error retrieving record:", ex);
 
-            Console.WriteLine("\r\nPress enter to exit");
-            Console.ReadLine();
+            }
+
         }
+
+        public static bool RecordFormat(string format, string[] record, int position) 
+        {
+            // check if format matches 
+            // firstname, middle, last
+            if (record[position].Equals(format))
+            {
+                return true;
+            }
+            return false;
+        
+        }
+
     }
 }
